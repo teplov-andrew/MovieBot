c0cfe5d543c549eb37696f9aa73d712f98b98103
Andrew Teplov <69726878+teplov-andrew@users.noreply.github.com>
2023-10-28 11:11:04 +0300

    Update mocks_tests.py

--- mocks_tests.py
+++ mocks_tests.py
@@ -2,9 +2,15 @@ import pytest
 import telebot
 import pandas as pd
 from config import CFG
+from help_functions import clear_msg
 from unittest.mock import patch, Mock
-from bot import kino_guied, help, film_ser, primers, acrtist__1, rand_film
+from bot import kino_guied, help, film_ser, primers, acrtist__1, rand_film, search_films
 
+
+data = pd.read_csv(CFG.PATH_DATA, sep=',')
+data_of_films = list(data["movie"])
+for i in range(len(data_of_films)):
+    data_of_films[i] = clear_msg(data_of_films[i])
 
 class TestBot:
     @pytest.fixture(autouse=True)
@@ -40,7 +46,30 @@ class TestBot:
             acrtist__1(self.message)
             mocked.assert_called_once_with(self.message.chat.id, 'Введите полное имя актера/актрисы...\n⬇️⬇️⬇️', parse_mode='html')
 
+    def test_rand_film(self):
+        with patch('telebot.TeleBot.send_message') as mocked:
+            rand_film(self.message)
+            assert mocked.called
+            args, kwargs = mocked.call_args
+            assert clear_msg(args[1].split("[")[1].split("]")[0]) in data_of_films
 
+
+    @pytest.mark.parametrize("user_message,expected_substring", [
+        ("Матрица", f'●  Название: [Матрица]'), 
+        ("Хэллоуин", f'●  Название: [Хэллоуин]'),
+        ("Киану Ривз", "●  Имя: [Киану Ривз]"),
+        ("Бред Пит", "●  Имя: [Брэд Питт]"),
+        ("йагйруаушощцоашцуоащоцщоашллд", "Я не понял ваш запрос! Попробуйте написать название фильма на английском"),
+        
+    ])
+
+    def test_search_films(self, user_message, expected_substring):
+        self.message.text = user_message
+        with patch('telebot.TeleBot.send_message') as mocked:
+            search_films(self.message)
+            assert mocked.called
+            args, kwargs = mocked.call_args
+            assert expected_substring in args[1]
 
 if __name__ == '__main__':
     pytest.main()
